9df67bc4f4d7ca822e3e694671cb5933dfb2acb5
onlinekyne <kyne.santos@yahoo.ca>
2026-09-20 14:34:30 -0400

    Revert "Build Astro Scholar site foundation"
    This reverts commit f918acd25dcfd7a15f89ed33be4ccd457225436f.

--- src/utils/paths.ts
+++
@@ -1,6 +0,0 @@
-const base = import.meta.env.BASE_URL.replace(/\/+$/, '');
-
-export const url = (path: string) => {
-  if (/^(https?:|mailto:|tel:)/.test(path)) return path;
-  return `${base}${path.startsWith('/') ? path : `/${path}`}`;
-};
